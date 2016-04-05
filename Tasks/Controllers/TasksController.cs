dd14030750a5754e79691f0fbe701deaa779b5b4
Kate Ramsey <kateramsey@live.com>
2016-04-05 13:52:35 -0500

    Add controller and views for tasks model

---
+++ Tasks/Controllers/TasksController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tasks.Controllers
+{
+    public class TasksController : Controller
+    {
+        // GET: Tasks
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: Tasks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tasks/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        } 
+    }
+}
