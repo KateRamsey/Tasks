c12e0da6dfe3349238d1a7fa0f00034bf89b4f78
Kate Ramsey <kateramsey@live.com>
2016-04-06 08:02:02 -0500

    Set DisplayName attributes on the Task model

--- Tasks/Models/Task.cs
+++ Tasks/Models/Task.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,11 @@ namespace Tasks.Models
     public class Task
     {
         [Required]
+        [DisplayName("Your Name")]
         public string OwnerName { get; set; }
         [Required]
         public string Description { get; set; }
+        [DisplayName("Completed?")]
         public bool IsFinished { get; set; }
     }
 }
